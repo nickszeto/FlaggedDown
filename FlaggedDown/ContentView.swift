a1fded54564e710bb9308edb7325fdda1afb09cb
Nicholas Szeto <szetonick@gmail.com>
2021-08-05 20:41:53 +0800

    Initial Commit

---
+++ FlaggedDown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlaggedDown
+//
+//  Created by Nicholas Szeto on 5/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlaggedDown/FlaggedDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlaggedDownApp.swift
+//  FlaggedDown
+//
+//  Created by Nicholas Szeto on 5/8/21.
+//
+
+import SwiftUI
+
+@main
+struct FlaggedDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
